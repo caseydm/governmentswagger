7565ae45e9d1620cd62ecee608d5d51dbbedf7f9
Casey M <caseym@gmail.com>
2016-04-30 15:09:19 -0400

    added db relationship

--- app.py
+++ app.py
@@ -14,24 +14,41 @@ db = SQLAlchemy(app)
 
 
 # Models
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(80), unique=True)
+
+    def __init__(self, city=""):
+        self.city = city
+
+    def __repr__(self):
+        return self.city
+
+
 class Hotel(db.Model):
+    __tablename__ = 'hotels'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
-    city = db.Column(db.String(80))
     url = db.Column(db.String(80))
     parking = db.Column(db.String(200))
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+    city = db.relationship(Location, backref='Hotel')
 
-    def __init__(self, name="", city="", url="", parking=""):
+    def __init__(self, name="", url="", parking="", location_id=""):
         self.name = name
-        self.city = city
         self.url = url
         self.parking = parking
+        self.location_id = location_id
 
 db.create_all()
 
 # admin setup
 admin = Admin(app, name='GovSwag Admin', template_mode='bootstrap3')
 admin.add_view(ModelView(Hotel, db.session))
+admin.add_view(ModelView(Location, db.session))
 
 
 # Views
