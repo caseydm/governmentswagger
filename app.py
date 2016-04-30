6646195f3dc8c40f5740e46539b3f8c870ff1fc3
Casey M <caseym@gmail.com>
2016-04-29 21:51:23 -0400

    database updates

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
 
+
+# Models
+class Hotel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    city = db.Column(db.String(80))
+    url = db.Column(db.String(80))
+
+db.create_all()
+
+# Views
 @app.route('/')
 def index():
     return render_template('index.html')
