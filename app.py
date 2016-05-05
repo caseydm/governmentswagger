21717258d515f802cebb7e648d404118fdbd0432
Casey M <caseym@gmail.com>
2016-05-04 20:13:56 -0500

    separated models

--- app.py
+++ app.py
@@ -1,51 +1,15 @@
 from flask import Flask, render_template, redirect
-from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask.ext.stormpath import StormpathManager, current_user
+from models import db, Hotel, Location
 
+
+# app setup
 app = Flask(__name__)
-
-# config
 app.config.from_object('config')
-
-db = SQLAlchemy(app)
+db.init_app(app)
 stormpath_manager = StormpathManager(app)
-
-
-# Models
-class Location(db.Model):
-    __tablename__ = 'locations'
-
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(80), unique=True)
-    city_url_slug = db.Column(db.String(80), unique=True)
-    hotels = db.relationship('Hotel', backref='location', lazy='dynamic')
-
-    def __init__(self, city="", city_url_slug=""):
-        self.city = city
-        self.city_url_slug = city_url_slug
-
-    def __repr__(self):
-        return self.city
-
-
-class Hotel(db.Model):
-    __tablename__ = 'hotels'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    url = db.Column(db.String(80))
-    parking = db.Column(db.String(200))
-    resort_fee = db.Column(db.String(20))
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
-
-    def __init__(self, name="", url="", parking="", resort_fee="", location_id=""):
-        self.name = name
-        self.url = url
-        self.parking = parking
-        self.resort_fee = resort_fee
-        self.location_id = location_id
 
 
 # admin setup
@@ -62,7 +26,8 @@ admin = Admin(app, name='GovSwag Admin', template_mode='bootstrap3')
 admin.add_view(MyModelView(Hotel, db.session))
 admin.add_view(MyModelView(Location, db.session))
 
-# Views
+
+# views
 @app.route('/')
 def index():
     locations = Location.query.all()
---
+++ models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+# Models
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(80), unique=True)
+    city_url_slug = db.Column(db.String(80), unique=True)
+    hotels = db.relationship('Hotel', backref='location', lazy='dynamic')
+
+    def __init__(self, city="", city_url_slug=""):
+        self.city = city
+        self.city_url_slug = city_url_slug
+
+    def __repr__(self):
+        return self.city
+
+
+class Hotel(db.Model):
+    __tablename__ = 'hotels'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    url = db.Column(db.String(80))
+    parking = db.Column(db.String(200))
+    resort_fee = db.Column(db.String(20))
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+
+    def __init__(self, name="", url="", parking="", resort_fee="", location_id=""):
+        self.name = name
+        self.url = url
+        self.parking = parking
+        self.resort_fee = resort_fee
+        self.location_id = location_id
